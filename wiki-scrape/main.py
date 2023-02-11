0fc182c69ec071e7224bcab43f779a9d858ca121
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-02-11 11:14:03 +0800

    filter characters via regex

--- wiki-scrape/main.py
+++ wiki-scrape/main.py
@@ -1,42 +1,38 @@
 from utils import wiki_url
 import re
-import json
+from concurrent.futures import ThreadPoolExecutor
 
 
 def main():
-    temp_list: list[dict[str, str]] = []
-    alphabet_list = [chr(x).upper() for x in range(97, 123)]
-
-    # TODO filter these via regex
-    REGEX_FILTER = ["/Enemies",
-                    "/People",
-                    "Character",
-                    "Category:",
-                    "Thread:",
-                    "parents",
-                    "family",
-                    "shareholders",
-                    "babies",
-                    "guards",
-                    "clones"]
-
-    for letter in alphabet_list:
-        character = wiki_url(category="Characters", params=f"?from={letter}")
-        character_item = character.select("a.category-page__member-link")
-
-        for char in character_item:
-            temp_list.append({
-                'name': char['title'],
-                'url': char['href']
-            })
-
-        print(f"appended items from '{letter}'")
-
-        # orig 290
-
-    with open("test.json", "a+", encoding="utf-8") as f:
-        json.dump({"characters": temp_list}, f, indent=4)
+	temp_list: list[dict[str, str]] = []
+	alphabet_list = [chr(x).upper() for x in range(97, 123)]
+	
+	for letter in alphabet_list:
+		character = wiki_url(category="Characters", params=f"?from={letter}")
+		character_item = character.select("a.category-page__member-link")
+		
+		for char in character_item:
+			temp_list.append({
+				'name': char['title'],
+				'url': char['href']
+			})
+		
+		print(f"added items from '{letter}'")
+	
+	filter_list = ["/Enemies", "/People", "Character", "Category:", "Thread:",
+				   "parents", "family", "shareholders", "babies", "guards", "clones", "lookalike"]
+	regex_filter = rf"({')|('.join(filter_list)})"
+	
+	for char in temp_list:
+		name = char["name"]
+		matches = re.findall(regex_filter, name, re.IGNORECASE)
+		
+		if not matches:
+			print(f"[YES] {name}")
+		else:
+			print(f"[---] {name}")
 
 
 if __name__ == "__main__":
-    main()
+	with ThreadPoolExecutor(max_workers=200) as e:
+		e.map(main(), range(250))  # NOQA
