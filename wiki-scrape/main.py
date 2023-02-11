b85dc5a34b2def572a99bec51315a1f7d8d08fb2
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-02-11 23:13:30 +0800

    add stuff to cache.json to memoize contents

--- wiki-scrape/main.py
+++ wiki-scrape/main.py
@@ -1,27 +1,41 @@
+from concurrent.futures import ThreadPoolExecutor
 from utils import wiki_url
 import re
-from concurrent.futures import ThreadPoolExecutor
+import json
 
 
 def main():
 	temp_list: list[dict[str, str]] = []
 	alphabet_list = [chr(x).upper() for x in range(97, 123)]
 	
-	for letter in alphabet_list:
-		character = wiki_url(category="Characters", params=f"?from={letter}")
-		character_item = character.select("a.category-page__member-link")
+	try:
+		print("cache.json found, appending data...")
 		
-		for char in character_item:
-			temp_list.append({
-				'name': char['title'],
-				'url': char['href']
-			})
-		
-		print(f"added items from '{letter}'")
+		with open("cache.json", 'r') as fuck:
+			temp_list = json.load(fuck)["characters"]
 	
-	filter_list = ["/Enemies", "/People", "Character", "Category:", "Thread:",
-				   "parents", "family", "shareholders", "babies", "guards", "clones", "lookalike"]
-	regex_filter = rf"({')|('.join(filter_list)})"
+	except FileNotFoundError:
+		print("cache.json not found, will generate anyway")
+		
+		for letter in alphabet_list:
+			character = wiki_url(category="Characters", params=f"?from={letter}")
+			character_item = character.select("a.category-page__member-link")
+			
+			for char in character_item:
+				temp_list.append({
+					'name': char['title'],
+					'url': char['href']
+				})
+			
+			print(f"added items from letter {letter}")
+		
+		with open("cache.json", "a+") as f:
+			json.dump({'characters': temp_list}, f, indent=4, ensure_ascii=True)
+	
+	prefixes = ["/Enemies", "/People", "Character", "Category:", "Thread:"]
+	categories = ["parents", "family", "shareholders", "babies", "guards", "clones", "lookalike"]
+	
+	regex_filter = rf"({')|('.join([*prefixes, *categories])})"
 	
 	for char in temp_list:
 		name = char["name"]
